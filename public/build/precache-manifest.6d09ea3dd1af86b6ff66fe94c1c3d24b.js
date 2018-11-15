4aa8502c46a1b6e05e57cda12a6747b322d9edec
Othmane Necib <othmane.necib@ars.de>
2018-11-15 11:47:43 +0100

    change theme

---
+++ public/build/precache-manifest.6d09ea3dd1af86b6ff66fe94c1c3d24b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1ba60b6572ea040e42a",
+    "url": "/static/js/main.c1ba60b6.chunk.js"
+  },
+  {
+    "revision": "95b48ea7f84fe400e923",
+    "url": "/static/js/1.95b48ea7.chunk.js"
+  },
+  {
+    "revision": "c1ba60b6572ea040e42a",
+    "url": "/static/css/main.d0a610fc.chunk.css"
+  },
+  {
+    "revision": "2d1214c21d7883ba574d264141f57c40",
+    "url": "/index.html"
+  }
+];
